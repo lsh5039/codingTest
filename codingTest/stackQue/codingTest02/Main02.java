002d87f3187a631aa9881520251e82e77ef7ca54
lsh5039 <lsh503984@gmail.com>
2021-04-13 21:49:20 +0900

    sorting 1 commit

--- codingTest/stackQue/codingTest02/Main02.java
+++ codingTest/stackQue/codingTest02/Main02.java
@@ -2,6 +2,8 @@ package codingTest02;
 
 import java.util.Arrays;
 
+import codingTest03.Main03;
+
 public class Main02 {
 	public static void main(String[] args) {
 		int [] prices = {1, 2, 3, 2, 3};
@@ -40,7 +42,6 @@ public class Main02 {
 	        	}
 	        		
 	        }
-	        
 	        
 	    }
 }
